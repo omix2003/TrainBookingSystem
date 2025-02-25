b696d9dfb68c869ffe4a452b0d75050b6f3f863f
Omkar Binage <omkarbinage0@gmail.com>
2025-02-25 22:54:44 +0530

    commiting unsatged

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port:3000,
-    proxy: {
-      '/api': 'http://localhost:8800'
-    }
-  },
   plugins: [react()],
-})
+
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8800",
+        changeOrigin: true,
+      },
+    },
+  },
+});
