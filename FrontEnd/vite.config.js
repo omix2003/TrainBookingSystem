cc99b7b065bbe46f3d57f35fc1a896224b614309
Omkar Binage <99659981+omix2003@users.noreply.github.com>
2025-02-28 22:53:07 +0530

    Update vite.config.js

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8800",
+        target: "https://trainbookingsystem-fe.onrender.com",
         changeOrigin: true,
       },
     },
