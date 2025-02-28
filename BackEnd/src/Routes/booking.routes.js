178ebe59939e0aff6bbe9dbafddc5c9344ac0885
Omkar Binage <omkarbinage0@gmail.com>
2025-02-28 23:17:22 +0530

    getting package.json file in root directory

--- BackEnd/src/Routes/booking.routes.js
+++ BackEnd/src/Routes/booking.routes.js
@@ -7,7 +7,7 @@ const router = Router()
 router.route("/bookings").post( verifyJWT,createBooking)
 router.route("/booking-details/c/:pnrNo").get(getBookingDetails)
 router.route("/cancel-booking/c/:pnrNo").post(verifyJWT, cancelBooking)
-router.route("/get-pnr").get( getAllPNR)
+router.route("/get-pnr").get(verifyJWT, getAllPNR)
 router.route("/get-all-booking-details").get( isAdmin,getAllBookingDetails)
 router.route("/get-booking-by-user").get(verifyJWT,getBookingsbyUser)
 export default router
