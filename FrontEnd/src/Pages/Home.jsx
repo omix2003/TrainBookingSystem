28df668984b9fbfacb16a32d4173d319f843a57d
Omkar Binage <omkarbinage0@gmail.com>
2025-02-28 23:46:16 +0530

    updating home.jsx

--- FrontEnd/src/Pages/Home.jsx
+++ FrontEnd/src/Pages/Home.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import { Header, Optionbar } from '../Components/index.js'
-import AboutUs from '../Components/AboutUs.jsx'
+import React from 'react';
+import { Header, Optionbar } from '../Components/index.js';
+import AboutUs from '../Components/AboutUs.jsx';
+import backgroundImage from "../assets/photos/background1.jpg"; // ✅ Ensure correct file extension
 
 export default function Home() {
     return (
         <>
             <div className='w-full h-screen flex flex-col'>
                 <Header />
-                <div className='w-full h-screen bg-[url("../../photos/background1.jpg")] bg-cover bg-opacity-50 bg-no-repeat p-0 m-0' >
+                {/* ✅ Correct way to use background image */}
+                <div
+                    className="w-full h-screen bg-cover bg-opacity-50 bg-no-repeat p-0 m-0"
+                    style={{ backgroundImage: `url(${backgroundImage})` }}
+                >
                     <Optionbar />
                 </div>
                 <AboutUs />
             </div>
         </>
-    )
+    );
 }
