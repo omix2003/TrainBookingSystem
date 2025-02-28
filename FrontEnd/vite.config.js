8a1cf93e177356a4ddc0e5c79b5da8dcf828c738
Omkar Binage <99659981+omix2003@users.noreply.github.com>
2025-03-01 00:38:28 +0530

    Update vite.config.js

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,17 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-
   server: {
-    port: 3000,
     proxy: {
-      "/api": {
-        target: "http://localhost:8800",
-        changeOrigin: true,
-      },
-    },
+      '/api': 'http://localhost:8800'
+    }
   },
-});
+  plugins: [react()],
+})
