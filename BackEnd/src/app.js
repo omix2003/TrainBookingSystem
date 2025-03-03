5e32bade54bf405bbd0a7f0ef43c56326f951c5f
Omkar Binage <99659981+omix2003@users.noreply.github.com>
2025-03-03 10:28:39 +0530

    Update app.js

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -4,11 +4,11 @@ import cookieParser from "cookie-parser";
 
 const app = express()
 
-app.use(cors({
-    origin: ["https://trainbookingsystem-fe.onrender.com"],
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    credentials: true
-}))
+// app.use(cors({
+//     origin: ["https://trainbookingsystem-fe.onrender.com"],
+//     methods: ["GET", "POST", "DELETE", "PUT"],
+//     credentials: true
+// }))
 
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
